0b945d4ba989c5c4c75e69f4c2ebf2fd2fac7f4d
Roberto <roberto.elbeto7@gmail.com>
2023-03-13 15:15:19 -0600

    Acomodo carpetas

---
+++ 2 Parcial/E Modulos/areas.js
@@ -0,0 +1,10 @@
+function areaCuadrado(lado){
+    return lado*lado
+}
+
+function areaTriangulo(base, altura){
+    return (base*altura)/2
+}
+
+module.exports.areaCuadrado = areaCuadrado;
+module.exports.areaTriangulo = areaTriangulo;
---
+++ 2 Parcial/E Modulos/index.js
@@ -0,0 +1,1 @@
+require('areas.js')
