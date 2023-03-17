93e2dd931d06e87b931288232b077f577abd3a43
Roberto <roberto.elbeto7@gmail.com>
2023-03-17 09:34:22 -0500

    Horario Json

--- 2 Parcial/G Horario Json/main.js
+++ 2 Parcial/G Horario Json/main.js
@@ -1,10 +1,19 @@
 window.onload = function() {
 
-    encabezados = ["NOMBRE", "APELLIDO1", "APELLIDO2"];
-    datos = [{  nombre: "Roberto",
-                apellido: "Lopez",
-                apellido2: "Morales"
-
+    encabezados = ["Hora", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
+    datos7am = [{  hora: "7-8",
+                lunes: "Programacion web",
+                martes: "Programacion web",
+                miercoles: "Programacion web",
+                jueves: "Programacion web",
+                viernes: "Programacion web"
+    }]
+    datos8am = [{  hora: "8-9",
+                lunes: "",
+                martes: "Taller de Sistemas operativos",
+                miercoles: "",
+                jueves: "Taller de Sistemas operativos",
+                viernes: ""
     }]
 
     let table = document.createElement('table');
@@ -19,18 +28,51 @@ window.onload = function() {
         tr.appendChild(th);
     });
 
-    datos.forEach(function(item){
+    datos7am.forEach(function(item){
         let tr = document.createElement("tr");
-        let td1 = document.createElement("td");
-        td1.innerText = item.nombre;
+        let th = document.createElement("th");
+        th.innerText = item.hora;
         let td2 = document.createElement("td");
-        td2.innerText = item.nombre;
+        td2.innerText = item.lunes;
         let td3 = document.createElement("td");
-        td3.innerText = item.nombre;
+        td3.innerText = item.martes;
+        let td4 = document.createElement("td");
+        td4.innerText = item.miercoles;
+        let td5 = document.createElement("td");
+        td5.innerText = item.jueves;
+        let td6 = document.createElement("td");
+        td6.innerText = item.viernes;
 
-        tr.appendChild(td1);
+        tr.appendChild(th);
         tr.appendChild(td2);
         tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+        tr.appendChild(td6);
+        tbody.appendChild(tr);
+    });
+
+    datos8am.forEach(function(item){
+        let tr = document.createElement("tr");
+        let th = document.createElement("th");
+        th.innerText = item.hora;
+        let td2 = document.createElement("td");
+        td2.innerText = item.lunes;
+        let td3 = document.createElement("td");
+        td3.innerText = item.martes;
+        let td4 = document.createElement("td");
+        td4.innerText = item.miercoles;
+        let td5 = document.createElement("td");
+        td5.innerText = item.jueves;
+        let td6 = document.createElement("td");
+        td6.innerText = item.viernes;
+
+        tr.appendChild(th);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+        tr.appendChild(td6);
         tbody.appendChild(tr);
     });
     thead.appendChild(tr);
