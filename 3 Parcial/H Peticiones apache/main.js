bb0ac42fefba7716d350afb1b73dc3c36275c1cf
Roberto <roberto.elbeto7@gmail.com>
2023-05-22 07:55:35 -0500

    acomodo carpetas

--- 3 Parcial/H Peticiones apache/main.js
+++ 3 Parcial/H Peticiones apache/main.js
@@ -1,14 +1,48 @@
 window.onload=function(){
+    //PETICION
     document.getElementById("btnPeticion").addEventListener("click", function(e){
+        e.preventDefault();    
+        fetch('http://localhost:8080/jugador')
+        .then(respuesta => respuesta.json())
+            .then(objetoJugador => {
+                console.log(objetoJugador);
+            })
+    });
+    //BUSCAR
+    document.getElementById("btnBuscar").addEventListener("click", function(e){
+        e.preventDefault();
+        
+        let idJug = prompt("Teclee el id del jugador");
+        let idBuscar= idJug-1;
+    
+        fetch('http://localhost:8080/jugador?idJugador=' + idJug)
+        .then(respuesta => respuesta.json())
+            .then(objetoJugador => {
+                console.log(objetoJugador);
+                document.getElementById("idJugador").value = objetoJugador[idBuscar].idJugador;
+                document.getElementById("nombre").value = objetoJugador[idBuscar].nombre;
+                document.getElementById("apellidos").value = objetoJugador[idBuscar].apellidos;
+                document.getElementById("fechaN").value = objetoJugador[idBuscar].fechaN;
+                document.getElementById("playera").value = objetoJugador[idBuscar].playera;
+                document.getElementById("equipo").value = objetoJugador[idBuscar].equipo;
+            })
+    });
+    //ELIMINAR
+    document.getElementById("btnEliminar").addEventListener("click", function(e){
         e.preventDefault();
         
         let idJugador = prompt("Teclee el id del jugador");
     
-        fetch('http://localhost:8085/jugador?idJugador=' + idJugador)
+        fetch('http://localhost:8080/jugador?idJugador=' + idJugador, {method: 'DELTE'})
         .then(respuesta => respuesta.json())
             .then(objetoJugador => {
-                console.log(objetoJugador);
-                document.getElementById("nombre").value = objetoJugador[0].nombre;
+                if(objetoJugador = )
+                document.getElementById("idJugador").value = '';
+                document.getElementById("nombre").value = '';
+                document.getElementById("apellidos").value = '';
+                document.getElementById("fechaN").value = '';
+                document.getElementById("playera").value = '';
+                document.getElementById("equipo").value = '';
             })
     });
 }
