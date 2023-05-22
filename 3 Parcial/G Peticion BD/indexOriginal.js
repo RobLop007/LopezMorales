d608747c53691ebd09bf1bf5ed75bdc7a6d6d9c8
Roberto <roberto.elbeto7@gmail.com>
2023-05-22 07:55:47 -0500

    acomodo carpetas

--- 3 Parcial/G Peticion BD/indexOriginal.js
+++ 3 Parcial/G Peticion BD/indexOriginal.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const  mysql = require('mysql');
 
+app.use(cors());
 app.use(express.json());
 
+//PETICION
 app.get('/jugador', (req, res) => {
     var conexion = mysql.createConnection({
       host      : 'localhost',
@@ -13,17 +16,75 @@ app.get('/jugador', (req, res) => {
     });
   
     conexion.connect();
-    conexion.query(`SELECT * FROM jugador `, function(error, results, fields){
+    conexion.query(`SELECT * FROM jugador`, function(error, results, fields){
       if (error) return res.json(error);      
       res.json(results);
     })
-    conexion.end();
+  conexion.end();
 }); 
+
+//BUSCAR
+app.get('/jugador/:', (req, res) => {
+  var conexion = mysql.createConnection({
+    host      : 'localhost',
+    user      : 'root',
+    password  : '',
+    database  : 'ejemploweb'
+  });
+  conexion.connect();
+  conexion.query(`SELECT * FROM jugador WHERE id=${idJugador}`, function(error, results, fields){
+    if (error) return res.json(error);      
+    res.json(results);
+  })
+conexion.end();
+}); 
+
+//ELIMINAR
+app.delete('/jugador/:', (req, res) => {
+    var conexion = mysql.createConnection({
+      host      : 'localhost',
+      user      : 'root',
+      password  : '',
+      database  : 'ejemploweb'
+    });
+    conexion.connect();
+    conexion.query(`DELETE  FROM jugador WHERE idJugador = ${idJugador}`, function(error, results, fields){
+      if (error) return res.json(error);      
+      res.json(results);
+    })
+  conexion.end();
+}); 
+
+//AGREGAR
+app.post('/jugador', (req, res) => {
+  var conexion = mysql.createConnection({
+    host      : 'localhost',
+    user      : 'root',
+    password  : '',
+    database  : 'ejemploweb'
+  });
+  let miInsert = "INSERT INTO jugador("
+                                    "'" + req.body.noombre + "',"   +
+                                    "'" + req.body.apellidos + "'," +
+                                    "'" + req.body.fechaN + "',"    +
+                                    "" + req.body.playera + ","   +
+                                    "'" + req.body.equipo + "')";
+          console.log(miInsert);
+          conexion.connect();
+  conexion.query(miInsert, function(error, results, fields){
+  if (error) return res.json(error);
+  else{console.log(results);
+          res.json(results);}      
+    res.json(results);
+  })
+conexion.end();
+}); 
+
 
 app.post('/jugador', (req, res) => {
   res.send('Peticion POST a jugador');
 });
 
-app.listen(8085, () => {
+app.listen(8080, () => {
   console.log(`Example app listening on port`);
 });
